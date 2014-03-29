4d74e7e0d1ad694fa5ab77c35791964fbdc4fc37
gperson <gperson@xpanxion.com>
2014-03-28 22:14:03 -0500

    Added slide show, animation to slide them is in progress

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 /**
-  * index.js
-  */
+ * index.js
+ */
 
 /**
-  * Runs when document has loaded.
-  */
+ * Runs when document has loaded.
+ */
 $(document).ready(function() {
+	//All code
+	var slider = document.getElementById('slider-content'); 
+	sliders = slider.children;
+	numSlides = sliders.length; //No of items in the above array
 	
+	sliderContent = document.getElementById('slider-content'); //#slider-content
+	sliderContent.style.width = (numSlides * 100) + "%";
+	
+	width = (100 / numSlides) + "%";
+	for (var i=0;i<numSlides;i++)
+	{ 
+		sliders[i].style.width = width;
+	}
+	setInterval(function (){slide()}, 5000);
 });
+
+function slide() {
+    $("#slider").animate({ "left": "-=50px" }, "slow" );
+}
