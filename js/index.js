1c3b45cee33345fa0b1eb1fbd6a479f415776dce
gperson <gperson@xpanxion.com>
2014-04-01 15:58:34 -0500

    Added header logo and animation

--- js/index.js
+++ js/index.js
@@ -11,5 +11,15 @@ var slideshow;
  * Runs when document has loaded.
  */
 $(document).ready(function() {
+	titleAnimation();
 	slideshow = new Slideshow('slider-content');
 });
+
+/**
+ * Moves the "Timo's" logo in from the left and "Detailing" link in from the right
+ */
+function titleAnimation() {
+	$('#timos').animate({left:'463px'},500,'linear');
+	$('#detailing').animate({right:'340px'},500,'linear');
+}
+	
