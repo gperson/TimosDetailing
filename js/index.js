2eb029bdfaa81c6b0f08e005b724be70bfa4ec13
Tony Lopez <tony.lopez1013@gmail.com>
2014-04-02 02:46:07 -0500

    Added mock content and other updates
    - Updated title animation
    - Removed mobile styling (not functional anyway)
    - Added mock content as well as content provided by stakeholder.
    - Defined some custom styles for common objects.
    - Updated styling for existing components

--- js/index.js
+++ js/index.js
@@ -11,15 +11,44 @@ var slideshow;
  * Runs when document has loaded.
  */
 $(document).ready(function() {
-	titleAnimation();
+	pageMethods.doIntroAnimation();
+	
 	slideshow = new Slideshow('slider-content');
+	
+	$("#nav a.navItem").click(function() {
+		var tabId = "navTab_" + $(this).text();
+		$("#page_content .navTab").hide();
+		$("#" + tabId).show();
+		
+		$("#nav a.navItem").removeClass("selected");
+		$(this).addClass("selected");
+	});
 });
+
+var pageMethods = {
+	doIntroAnimation : function() {
+		var STEP_SPEED = 300;
+		$("#header").fadeTo(STEP_SPEED, 1, function() {
+			$("#header .headerCarImage").fadeTo(STEP_SPEED, 1, function() {
+				$("#header .us_states_flag").fadeTo(STEP_SPEED, 1, function() {
+				$('#header .title_wrapper .image_timos').animate({right:'195px'}, STEP_SPEED, function() {
+					$('#header .title_wrapper .image_detailing').animate({left:'709px'}, STEP_SPEED, function() {
+						$("#main_section").fadeTo(STEP_SPEED * 5, 1);
+						$("#footer").fadeTo(STEP_SPEED * 5, 1);
+					});
+				});
+				});
+			});
+		});
+	}
+};
 
 /**
  * Moves the "Timo's" logo in from the left and "Detailing" link in from the right
  */
 function titleAnimation() {
-	$('#timos').animate({left:'463px'},500,'linear');
-	$('#detailing').animate({right:'340px'},500,'linear');
+	$('#header .title_wrapper .image_timos').animate({right:'285px'}, 300, function() {
+		$('#header .title_wrapper .image_detailing').animate({left:'598px'},300,'linear');
+	});
 }
 	
